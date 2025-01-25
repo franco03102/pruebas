5f77775186b08fc1435683a48261fac861f22362
Franco Moncayo Uribe <francomoncayo123@gmail.com>
2025-01-25 10:40:26 -0500

    correo
    Creación de la función para enviar el correo con parámetros locales

--- Prueba/app.py
+++ Prueba/app.py
@@ -3,7 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import time
+import time, base64, smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 #INICIO DE PROCESO
 data = 2
@@ -23,7 +25,7 @@ while data != 1:
                 #CONFIGURACIÓN DE DESCARGA EN EL NAVEGADOR
                 settings = Options()
                 settings.add_experimental_option(
-                    "prefs", {"download.default_directory":"C:\\Users\\franc\\OneDrive\\Documentos\\GitHub\\pruebas"}
+                    "prefs", {"download.default_directory":"C:\\Users\\franc\\OneDrive\\Documentos\\GitHub\\pruebas\\Archivos\\Original"}
                 )
 
                 #INICIO DEL NAVEGADOR CON SUS CONFIGURACIONES
@@ -41,9 +43,50 @@ while data != 1:
 
                 init.close()
 
+            def envioEmail():
+
+                #ENCRIPTACIÓN DE CONTRASEÑA EN BASE64
+                encode = b'ZnNyZCB0dmVuIGZhY3IgeWFicw=='
+                data = base64.b64decode(encode)
+
+                #TRANSOFMACIÓN DE INFORMACIÓN EN FORMATO UTF-8
+                password = data.decode('utf-8')
+
+                #CONFIGURACIÓN DEL SERVIDOR
+                smtp_server = "smtp.gmail.com"
+                smtp_port = 587
+                smtp_username = 'francosecundaria@gmail.com'
+                smtp_password = password
+
+                #CONFIGURACIÓN DEL MENSAJE
+                sender_email = 'francosecundaria@gmail.com'
+                receiver_email = 'francomoncayo123@gmail.com'
+                # subject = 'INTEGRACIÓN JOOR-DYNAMICS ' + str(file)
+                subject = "prueba"
+                body = "Hola"
+
+                message = MIMEMultipart()
+                message['From'] = sender_email
+                message['To'] = receiver_email
+                message['Subject'] = subject
+                message.attach(MIMEText(body, 'plain'))
+
+                context = ssl.create_default_context()
+
+                #CONEXIÓN CON EL SERVIDOR
+                server =  smtplib.SMTP(smtp_server, smtp_port)
+                server.starttls(context=context)
+                server.login(smtp_username, smtp_password)
+                server.sendmail(sender_email, receiver_email, message.as_string())
+
+                print("Correo electrónico enviado exitosamente.")
+
+        
+
         def main():
 
-            Prueba.descargaArchivo()
+            # Prueba.descargaArchivo()
+            Prueba.envioEmail()
 
         if __name__ == "__main__":
 
